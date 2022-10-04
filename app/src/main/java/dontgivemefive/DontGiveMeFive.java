36fe5d4591c83331d9ca691a15091af13f2cc782
charlotte buchan <cebuc@talktalk.net>
2022-10-04 15:37:25 +0100

    all tests passing

--- app/src/main/java/dontgivemefive/DontGiveMeFive.java
+++ app/src/main/java/dontgivemefive/DontGiveMeFive.java
@@ -1,10 +1,19 @@
 package dontgivemefive;
 
+import java.util.ArrayList;
+
 public class DontGiveMeFive {
 
     public int dontGiveMeFive(int start, int end){
+    int[] getValuesBetween = new int[(end + 1) - start];
+    ArrayList<Integer> storeValues = new ArrayList<>();
+    for(int i = 0; i < getValuesBetween.length; i++){
+        if(i % 5 != 0 ) {
+            storeValues.add(getValuesBetween[i] + 1);
+        }
 
-        return 4;
+    }
+        return storeValues.size() + 1;
 
     }
 }
--- app/src/test/java/dontgivemefive/DontGiveMeFiveTest.java
+++ app/src/test/java/dontgivemefive/DontGiveMeFiveTest.java
@@ -10,6 +10,26 @@ public class DontGiveMeFiveTest {
        DontGiveMeFive neverGiveMeFive = new DontGiveMeFive();
        assertEquals(4, neverGiveMeFive.dontGiveMeFive(1,4));
     }
+    @Test
+    public void givenARangeBetween1And9DontGiveMeFiveCountToBe8(){
+        DontGiveMeFive neverGiveMeFive = new DontGiveMeFive();
+        assertEquals(8, neverGiveMeFive.dontGiveMeFive(1,9));
+    }
+    @Test
+    public void givenARangeBetween4And17DontGiveMeFiveCountToBe12(){
+        DontGiveMeFive neverGiveMeFive = new DontGiveMeFive();
+        assertEquals(12, neverGiveMeFive.dontGiveMeFive(4,17));
+    }
+    @Test
+    public void givenARangeBetweenNegative1AndNegative10DontGiveMeFiveCountToBe7(){
+        DontGiveMeFive neverGiveMeFive = new DontGiveMeFive();
+        assertEquals(9, neverGiveMeFive.dontGiveMeFive(-10,-1));
+    }
+    @Test
+    public void givenARangeBetweenNegative1AndNegative20DontGiveMeFiveCountToBe17(){
+        DontGiveMeFive neverGiveMeFive = new DontGiveMeFive();
+        assertEquals(17, neverGiveMeFive.dontGiveMeFive(-20,-1));
+    }
 }
 
-//(1,9)  8
+
